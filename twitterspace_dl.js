468cf0151f0608a39874cd412fe6effad7a9eae4
ioio920430@gmail.com <ioio920430@gmail.com>
2022-06-16 18:53:46 +0800

    Fix broadcastId 403 BUG again and again

--- twitterspace_dl.js
+++ twitterspace_dl.js
@@ -388,7 +388,7 @@ async function TwitterSpace(whoseSpace, configObj) {
                 "responsive_web_uc_gql_enabled": false,
                 "vibe_tweet_context_enabled": false,
                 "responsive_web_edit_tweet_api_enabled": false,
-                "standardized_nudges_for_misinfo_nudges_enabled": false,
+                "standardized_nudges_misinfo": false,
                 "responsive_web_enhance_cards_enabled": false
             })), {
             "headers": {
