1cef5be513ca70a96d6e78370fcca8a62235572a
ioio920430@gmail.com <ioio920430@gmail.com>
2022-06-26 18:03:09 +0800

    add features token and add noCheck option

--- GetQueryId.js
+++ GetQueryId.js
@@ -1,26 +1,34 @@
+
 import axios from "axios";
 import fs from "fs";
 
-async function GetQueryId(QraphlName) {
+async function GetQueryId(QraphlName, noCheck) {
 
-    //Get mainJsId for request Url
-    let mainJsId = await axios("https://twitter.com", {
-        "headers": {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36"
-        },
-        "method": "GET"
-    })
-        .then((response) => { return response.data.match(/(?<=web\/main\.).*?(?=\.js)/)[0]; })
-        .catch(() => { console.log('get mainJsId fail.'); return -1; });
 
-    if (mainJsId === -1) { return -1; }
-
-    //Check main.xxxxxxxx.js is newest or not. If is newest then skip download main.js and output.
+    let mainJsId = "";
     let QueryIdListData = "";
     try { QueryIdListData = JSON.parse(fs.readFileSync(`./QueryIdList.json`)); }
     catch (err) { console.log('Failed to load QueryIdList.json now clear old file and rebuild one.'); fs.writeFileSync(`./QueryIdList.json`, JSON.stringify({})); }
 
-    if (QueryIdListData.mainJsId != mainJsId) {
+    if (!noCheck) {
+        //Get mainJsId for request Url
+        console.log('a');
+        mainJsId = await axios("https://twitter.com", {
+            "headers": {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36"
+            },
+            "method": "GET"
+        })
+            .then((response) => { return response.data.match(/(?<=web\/main\.).*?(?=\.js)/)[0]; })
+            .catch(() => { console.log('get mainJsId fail.'); return -1; });
+
+        if (mainJsId === -1) { return -1; }
+    }
+    //Check main.xxxxxxxx.js is newest or not. If is newest then skip download main.js and output.
+
+
+    if (QueryIdListData.mainJsId != mainJsId && !noCheck) {
+        console.log('b');
 
         //Prase main.xxxxxxxx.js file to get queryId 
         let mainJsUrl = 'https://abs.twimg.com/responsive-web/client-web/main.' + mainJsId + '.js';
@@ -32,27 +40,47 @@ async function GetQueryId(QraphlName) {
         })
             .then((response) => {
 
-                let rawList = response.data.match(/queryId:"([^"]+)",operationName:"([^"]+)"?(?=,operationType:"query")/g);
+                let rawList = response.data.match(/queryId\:\"([^"]+)\"\,operationName\:\"([^"]+)\"\,operationType\:\"query\"\,metadata\:\{featureSwitches\:\[([^\[\]]+)?(?=\]\}\}\})/g);
 
                 let operateName = [];
                 let operateId = [];
+                let queryRawList = [];
+                let operateQuery = {};
 
                 for (let i = 0, operate = ""; i < rawList.length; i++) {
 
                     operate = rawList[i];
+
                     operateId.push(operate.match(/(?<=queryId:").*?(?=")/)[0]);
                     operateName.push(operate.match(/(?<=operationName:").*?(?=")/)[0]);
+                    if (operate.match(/(?<=featureSwitches:\[).*/)[0] !== "") {
+                        queryRawList = operate.match(/(?<=featureSwitches:\[).*/)[0].match(/(?<=\")([^",]+)(?=\")/g);
+                    }
+                    else {
+                        queryRawList = [""];
+                    }
+                    operateQuery[operateName[i]] = [];
+                    for (let j = 0; j < queryRawList.length; j++) {
+                        operateQuery[operateName[i]].push(queryRawList[j]);
+                    }
 
                 }
 
-                let outputJson = { "mainJsId": mainJsId, "QueryId": {} };
+                let outputJson = { "mainJsId": mainJsId, "queryInfo": {} };
                 for (let i = 0; i < operateId.length; i++) {
-                    outputJson.QueryId[operateName[i]] = operateId[i];
+                    outputJson.queryInfo[operateName[i]] = {
+                        "queryId": operateId[i],
+                        "queryToken": operateQuery[operateName[i]]
+                    };
                 }
                 return outputJson;
 
             })
-            .catch(() => { console.log('get mainJsData fail.'); return -1; });
+            .catch((res) => {
+                console.log('get mainJsData fail.');
+                console.log(res);
+                return -1;
+            });
 
         if (QueryIdListData === -1) { return -1; }
 
@@ -67,13 +95,21 @@ async function GetQueryId(QraphlName) {
 
         for (let i = 0, operate = ""; i < QraphlName.length; i++) {
             operate = QraphlName[i];
-            returnArray[i] = QueryIdListData.QueryId[`${operate}`];
+            returnArray[i] = QueryIdListData.queryInfo[`${operate}`];
         }
 
         return returnArray;
 
     }
-    else if (typeof QraphlName === 'string') { return QueryIdListData.QueryId[`${QraphlName}`]; }
+    else if (typeof QraphlName === 'string') {
+
+        if (QraphlName === "all") {
+            return QueryIdListData;
+        }
+        else {
+            return QueryIdListData.queryInfo[`${QraphlName}`];
+        }
+    }
     else {
         console.log("Please only enter Array or string.");
         return -1;
@@ -82,7 +118,3 @@ async function GetQueryId(QraphlName) {
 }
 
 export default GetQueryId;
-
-
-
-
